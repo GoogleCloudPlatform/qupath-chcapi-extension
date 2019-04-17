5b87400715eeac581d5c422525c8d321e26b2444
Artyom Gordiyenko <artyom.gordienko@quantumsoft.ru>
2019-04-17 16:27:13 +0700

    Added license headers

--- src/main/java/com/quantumsoft/qupathcloud/converter/dicomizer/ImageToWsiDcmConverter.java
+++ src/main/java/com/quantumsoft/qupathcloud/converter/dicomizer/ImageToWsiDcmConverter.java
@@ -1,14 +1,26 @@
+// Copyright (C) 2019 Google LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.quantumsoft.qupathcloud.converter.dicomizer;
 
 import com.quantumsoft.qupathcloud.exception.QuPathCloudException;
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openslide.OpenSlide;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Map;
 
 public class ImageToWsiDcmConverter {
